37e3d7913dececfe5c40486568569c7c905453bc
Asmita Prajapati <asmitap1999@gmail.com>
2021-03-08 15:53:04 +0530

    updatedd

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -14,11 +14,9 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-
 @app.before_first_request
 def create_tables():
     db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
